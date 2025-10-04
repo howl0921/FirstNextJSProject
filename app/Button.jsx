ac894f9a7285c2490d97a5d9aae0add87fbc7ce7
HowlLee <howllee.cn@gmail.com>
2025-10-05 02:29:00 +0800

    cloudflare config

--- app/Button.jsx
+++ app/Button.jsx
@@ -1,10 +1,11 @@
-"use client"
-import {useState} from "react"
+"use client";
+import { useState } from "react";
 export default function Button() {
     const [likes, setLikes] = useState(0);
 
     function handleClick() {
         setLikes(likes + 1);
     }
-    return <button onClick={() => {handleClick()}}>like ({likes})</button>
+    
+    return <button onClick={ handleClick }> like ({ likes })</button>;
 }
